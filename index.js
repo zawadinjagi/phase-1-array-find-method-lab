3798b5fa235dcb5146af082049617b6753c82983
zawadinjagi <zawadinjagi619@gmail.com>
2024-03-25 11:27:45 +0300

    add

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-// code your solution here
+// index.js
+
+function superbowlWin(records) {
+    const winRecord = records.find(record => record.result === "W");
+    return winRecord ? winRecord.year : undefined;
+  }
+  
+  module.exports = superbowlWin;
+  
